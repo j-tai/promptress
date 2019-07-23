fa6840c32b840fd264f3f76de22d4a3a6117b203
Jasmine Tai <jtai@jtai.ca>
2019-07-23 14:48:48 -0700

    Initial commit

---
+++ src/config.rs
@@ -0,0 +1,134 @@
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Copy, Clone, Serialize, Deserialize)]
+#[serde(default)]
+pub struct Style {
+    pub color: u8,
+    pub bold: bool,
+    pub italic: bool,
+    pub strike: bool,
+}
+
+impl Style {
+    pub fn color(color: u8) -> Style {
+        Style {
+            color,
+            ..Default::default()
+        }
+    }
+
+    pub fn bold(color: u8) -> Style {
+        Style {
+            color,
+            bold: true,
+            ..Default::default()
+        }
+    }
+}
+
+#[derive(Default, Serialize, Deserialize)]
+#[serde(default)]
+pub struct Config<'a> {
+    pub dollar: Dollar,
+    pub exit_code: ExitCode,
+    #[serde(borrow)]
+    pub git: Git<'a>,
+    #[serde(borrow)]
+    pub work_dir: WorkDir<'a>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(default)]
+pub struct Dollar {
+    pub bg: u8,
+    pub user_sty: Style,
+    pub root_sty: Style,
+}
+
+impl Default for Dollar {
+    fn default() -> Self {
+        Dollar {
+            bg: 0,
+            user_sty: Style::color(15),
+            root_sty: Style::bold(9),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(default)]
+pub struct ExitCode {
+    pub success_bg: u8,
+    pub success_sty: Style,
+    pub failure_bg: u8,
+    pub failure_sty: Style,
+}
+
+impl Default for ExitCode {
+    fn default() -> Self {
+        ExitCode {
+            success_bg: 0,
+            success_sty: Style::bold(10),
+            failure_bg: 0,
+            failure_sty: Style::bold(9),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(default)]
+pub struct Git<'a> {
+    pub bg: u8,
+    pub sty: Style,
+    pub prefix: Cow<'a, str>,
+}
+
+impl Default for Git<'_> {
+    fn default() -> Self {
+        Git {
+            bg: 7,
+            sty: Style::color(0),
+            prefix: "Git:".into(),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(default)]
+pub struct WorkDir<'a> {
+    pub bg: u8,
+    pub sty: Style,
+    pub trun_bg: u8,
+    pub trun_sty: Style,
+    pub stem_bg: u8,
+    pub stem_sty: Style,
+    #[serde(borrow)]
+    pub trun: Cow<'a, str>,
+    #[serde(borrow)]
+    pub dir_trun: Cow<'a, str>,
+    pub max_len: usize,
+    pub dir_max_len: usize,
+    #[serde(borrow)]
+    pub aliases: HashMap<Cow<'a, str>, Cow<'a, str>>,
+}
+
+impl Default for WorkDir<'_> {
+    fn default() -> Self {
+        WorkDir {
+            bg: 15,
+            sty: Style::color(0),
+            trun_bg: 15,
+            trun_sty: Style::color(0),
+            stem_bg: 15,
+            stem_sty: Style::color(0),
+            trun: "...".into(),
+            dir_trun: "...".into(),
+            max_len: 64,
+            dir_max_len: 16,
+            aliases: Default::default(),
+        }
+    }
+}
---
+++ src/parts/workdir.rs
@@ -0,0 +1,89 @@
+use std::env;
+use std::path::{Component, Path, PathBuf};
+
+use smallvec::SmallVec;
+
+use crate::Prompt;
+
+pub fn work_dir(p: &mut Prompt) {
+    let dir: PathBuf = env::var_os("PWD")
+        .map(|s| s.into())
+        .unwrap_or_else(|| env::current_dir().expect("cannot get working directory"));
+
+    let mut longest_match = 0;
+    let mut new_path = None;
+    for (prefix, value) in &p.conf.work_dir.aliases {
+        let len = prefix.chars().count();
+        if len <= longest_match {
+            continue;
+        }
+        let prefix_path = Path::new(prefix.as_ref());
+        match dir.strip_prefix(prefix_path) {
+            Ok(path) => {
+                let value_path = Path::new(value.as_ref());
+                new_path = Some((value_path, path));
+                longest_match = len;
+            }
+            Err(_) => (),
+        }
+    }
+    let path = new_path.map(|(a, b)| a.join(b)).unwrap_or(dir);
+
+    let components: SmallVec<[_; 16]> = path.components().collect();
+    let total = components.len();
+    let mut begin = 0;
+
+    let mut length = 0;
+    let max_len = p.conf.work_dir.max_len;
+    for (index, comp) in components.iter().enumerate().rev() {
+        length += 3;
+        length += match comp {
+            Component::Prefix(_) => unimplemented!(),
+            Component::RootDir => 1,
+            Component::Normal(s) => s.to_string_lossy().chars().count(),
+            Component::CurDir => 1,
+            Component::ParentDir => 2,
+        }
+        .min(max_len);
+        if length > max_len {
+            begin = index + 1;
+        }
+    }
+
+    if begin != 0 {
+        p.new_part(p.conf.work_dir.trun_bg);
+        p.style(p.conf.work_dir.trun_sty);
+        print!("{}", p.conf.work_dir.trun);
+    }
+
+    let dir_trun_len = p.conf.work_dir.dir_trun.chars().count();
+    for (index, comp) in components[begin..].iter().enumerate() {
+        if index == total - 1 {
+            p.new_part(p.conf.work_dir.stem_bg);
+            p.style(p.conf.work_dir.stem_sty);
+        } else {
+            p.new_part(p.conf.work_dir.bg);
+            p.style(p.conf.work_dir.sty);
+        }
+        let s = match comp {
+            Component::Prefix(_) => unimplemented!(),
+            Component::RootDir => "/".into(),
+            Component::Normal(s) => s.to_string_lossy(),
+            Component::CurDir => ".".into(),
+            Component::ParentDir => "..".into(),
+        };
+        let len = s.chars().count();
+        if len > p.conf.work_dir.dir_max_len {
+            let new_len = p
+                .conf
+                .work_dir
+                .dir_max_len
+                .checked_sub(dir_trun_len)
+                .unwrap();
+            let end = s.char_indices().nth(new_len).unwrap().0;
+            print!("{}{}", &s[..end], p.conf.work_dir.dir_trun);
+        } else {
+            print!("{}", s);
+        }
+    }
+}
