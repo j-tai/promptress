64cc182d94c2284fafecabcf802fac1afd4debf2
Jasmine Tai <jtai@jtai.ca>
2019-10-13 22:02:40 -0700

    Allow displaying prompt without compiling config
    This allows the user to display the prompt without compiling the config.
    If no PROMPTRESS_CONFIG environment variable was found, then
    ~/.promptress.toml is read directly.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
+use std::path::Path;
 use std::env;
+use std::env::VarError;
 use std::fs::File;
 use std::io;
 use std::io::Read;
@@ -18,39 +20,70 @@ fn display_prompt(mut p: Prompt) {
     p.finish();
 }
 
+/// Reads from a file. If this fails, prints the error and exits the
+/// program.
+fn read_file(filename: impl AsRef<Path>) -> String {
+    let filename = filename.as_ref();
+    let mut file = match File::open(filename) {
+        Ok(f) => f,
+        Err(e) => {
+            eprintln!("promptress: cannot open {}: {}", filename.display(), e);
+            process::exit(1);
+        }
+    };
+    let mut s = String::new();
+    match file.read_to_string(&mut s) {
+        Ok(_) => {}
+        Err(e) => {
+            eprintln!("promptress: cannot read from {}: {}", filename.display(), e);
+            process::exit(1);
+        }
+    };
+    s
+}
+
 fn main() {
     let args: Vec<_> = env::args().skip(1).collect();
     if args.is_empty() {
-        let config = match env::var("PROMPTRESS_CONFIG") {
-            Ok(c) => c,
-            Err(_) => {
-                eprintln!("promptress: PROMPTRESS_CONFIG not set or contains invalid UTF-8");
+        // Display the prompt
+        match env::var("PROMPTRESS_CONFIG") {
+            Ok(conf) => {
+                let config: Config = match serde_json::from_str(&conf) {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("promptress: cannot parse PROMPTRESS_CONFIG: {}", e);
+                        process::exit(1);
+                    }
+                };
+                display_prompt(Prompt::new(config));
+            }
+            Err(VarError::NotPresent) => {
+                // If PROMPTRESS_CONFIG is not set, then read from .promptress.toml
+                let mut path = dirs::home_dir().expect("cannot get home dir");
+                path.push(".promptress.toml");
+                let conf = read_file(&path);
+                let config: Config = match toml::from_str(&conf) {
+                    Ok(c) => c,
+                    Err(e) => {
+                        eprintln!("promptress: cannot parse {}: {}", path.display(), e);
+                        process::exit(1);
+                    }
+                };
+                display_prompt(Prompt::new(config));
+            }
+            Err(VarError::NotUnicode(_)) => {
+                // Invalid Unicode -- just print the error and exit
+                eprintln!("promptress: PROMPTRESS_CONFIG contains invalid unicode");
                 process::exit(1);
             }
         };
-        let config: Config = match serde_json::from_str(&config) {
-            Ok(c) => c,
-            Err(e) => {
-                eprintln!("promptress: cannot parse PROMPTRESS_CONFIG: {}", e);
-                process::exit(1);
-            }
-        };
-        display_prompt(Prompt::new(config));
     } else if args.len() == 1 {
-        let filename = &args[0];
-        let mut file = match File::open(filename) {
-            Ok(f) => f,
-            Err(e) => {
-                eprintln!("promptress: cannot open {}: {}", filename, e);
-                process::exit(1);
-            }
-        };
-        let mut s = String::new();
-        file.read_to_string(&mut s).unwrap();
+        // Compile a config
+        let s = read_file(&args[0]);
         let config: Config = match toml::from_str(&s) {
             Ok(c) => c,
             Err(e) => {
-                eprintln!("promptress: cannot parse {}: {}", filename, e);
+                eprintln!("promptress: cannot parse {}: {}", args[0], e);
                 process::exit(1);
             }
         };
